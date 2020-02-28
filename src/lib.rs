819f14e971b4c47111b0cc8d1049f43cf032ca1e
Daniel Whitney <dwhitney@spellmanhv.co.uk>
2020-02-28 10:52:57 +0000

    complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert (s:&str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}", name))
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
